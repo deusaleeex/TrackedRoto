d722aff392c02ab564e6cda841a3f2c55a509e14
deusaleeex <158163816+deusaleeex@users.noreply.github.com>
2024-02-02 08:42:00 +0000

    Update TrackedRoto.py
    Added an option to live-link or bake the tracking data.

--- TrackedRoto.py
+++ TrackedRoto.py
@@ -1,43 +1,72 @@
 import nuke
 
 def link_roto(tracker_node):
+    
     # Create Roto node
     i = nuke.createNode("Roto")
     i.setInput(0, None)
+    i.setXYpos(tracker_node.xpos() + 50, tracker_node.ypos() + 50) 
 
+    link_method = tracker_node['which'].value()
+    
     # Link values if bool == true
     tran = tracker_node['t'].value()
     rot = tracker_node['r'].value()
     sca = tracker_node['s'].value()
-
-    if tran:
-        i['translate'].setExpression("parent." + tracker_node.name() + ".translate")
-    if rot:
-        i['rotate'].setExpression("parent." + tracker_node.name() + ".rotate")
-    if sca:
-        i['scale'].setExpression("parent." + tracker_node.name() + ".scale")
+    cen = tran or rot or sca
+    
+    if link_method == 'live-link':
+        if tran:
+            i['translate'].setExpression(tracker_node.name() + ".translate")
+        if rot:
+            i['rotate'].setExpression(tracker_node.name() + ".rotate")
+        if sca:
+            i['scale'].setExpression(tracker_node.name() + ".scale")
+        if cen:
+            i['center'].setExpression(tracker_node.name() + ".center")
+            
+    elif link_method == 'baked':
+        i['translate'].fromScript(tracker_node['translate'].toScript())
+        i['rotate'].fromScript(tracker_node['rotate'].toScript())
+        i['scale'].fromScript(tracker_node['scale'].toScript())
+        i['center'].fromScript(tracker_node['center'].toScript())
+        i['label'].setValue("Baked: " + tracker_node.name())
 
 def create_knobs():
     tracker_node = nuke.thisNode()
 
     tab = nuke.Tab_Knob('Create Roto')
     tracker_node.addKnob(tab)
+    
+    dat = nuke.Text_Knob("<b>Data")
+    dat.setFlag(nuke.STARTLINE)
+    tracker_node.addKnob(dat)
 
-    tran = nuke.Boolean_Knob('t', 'Translate')
+    tran = nuke.Boolean_Knob('t', 'translate',)
+    tran.setValue(True)
     tran.setFlag(nuke.STARTLINE)
     tracker_node.addKnob(tran)
 
-    rot = nuke.Boolean_Knob('r', 'Rotation')
+    rot = nuke.Boolean_Knob('r', 'rotation')
     rot.setFlag(nuke.STARTLINE)
     tracker_node.addKnob(rot)
 
-    sca = nuke.Boolean_Knob('s', 'Scale')
+    sca = nuke.Boolean_Knob('s', 'scale')
     sca.setFlag(nuke.STARTLINE)
     tracker_node.addKnob(sca)
-   
-    cr = nuke.PyScript_Knob('cr', 'Create Linked Roto', 'TrackedRoto.link_roto(nuke.thisNode())')
-    cr.setFlag(nuke.STARTLINE)
+    
+    expo = nuke.Text_Knob("<b>Export")
+    expo.setFlag(nuke.STARTLINE)
+    tracker_node.addKnob(expo)
+    
+    which = nuke.Enumeration_Knob('which', 'link method', ['live-link', 'baked'])
+    which.setFlag(nuke.STARTLINE)
+    tracker_node.addKnob(which)
+    
+    # Create Linked Roto button
+    cr = nuke.PyScript_Knob('cr', 'create', 'TrackedRoto.link_roto(nuke.thisNode())')
     tracker_node.addKnob(cr)
+
 
 # Add callback
 nuke.addOnUserCreate(create_knobs, nodeClass="Tracker4")
